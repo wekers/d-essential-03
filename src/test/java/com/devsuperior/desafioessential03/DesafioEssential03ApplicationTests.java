dc5952443cce1410dd4322d6d351bf2507d930a9
wekers <fernando@wekers.org>
2023-06-04 12:09:55 -0300

    CRUD de clientes

---
+++ src/test/java/com/devsuperior/desafioessential03/DesafioEssential03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioessential03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEssential03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
