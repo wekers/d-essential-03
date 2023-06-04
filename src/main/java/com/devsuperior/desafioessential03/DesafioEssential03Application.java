047d8dda1003ded366537f9b90d111ffc18dd890
wekers <fernando@wekers.org>
2023-06-04 12:11:48 -0300

    Classe Client

---
+++ src/main/java/com/devsuperior/desafioessential03/DesafioEssential03Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioessential03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEssential03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEssential03Application.class, args);
+	}
+
+}
