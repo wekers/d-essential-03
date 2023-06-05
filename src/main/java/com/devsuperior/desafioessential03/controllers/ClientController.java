71660393e582d22eef4ce28e54a761780c3d4989
wekers <fernando@wekers.org>
2023-06-04 21:09:38 -0300

    controller

---
+++ src/main/java/com/devsuperior/desafioessential03/controllers/ClientController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.desafioessential03.controllers;
+
+
+import com.devsuperior.desafioessential03.dto.ClientDTO;
+import com.devsuperior.desafioessential03.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ClientDTO findById(@PathVariable Long id) {
+        return service.findById(id);
+
+    }
+
+}
